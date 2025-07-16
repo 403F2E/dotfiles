df62c48c5943b2f77620783472e22a26972277b2
403f2e <shadowouss1@gmail.com>
2025-07-16 23:00:12 +0100

    feat: adding cava & tty-clock at start, adding gitsigns & fugitive for git manipulation inside nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,6 +4,19 @@ require("nvchad.mappings")
 
 local map = vim.keymap.set
 
+map(
+	"n",
+	"<leader>ff",
+	"<cmd>Telescope find_files follow=true  no_ignore=true hidden=true<cr>",
+	{ desc = "telescope find files" }
+)
+map(
+	"n",
+	"<leader>fa",
+	"<cmd>Telescope find_files follow=true cwd=~/ no_ignore=true hidden=true<CR>",
+	{ desc = "telescope find all files" }
+)
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("i", "jk", "<ESC>")
@@ -19,3 +32,21 @@ map("n", "<leader>dn", vim.diagnostic.goto_next, { desc = "Go to Next Error" })
 map("n", "<leader>mp", ":MarkdownPreview <CR>", { desc = "Markdown Preview" })
 map("n", "<leader>ms", ":MarkdownPreviewStop <CR>", { desc = "Markdown Preview Stop" })
 map("n", "<leader>mt", ":MarkdownPreviewToggle <CR>", { desc = "Markdown Preview Toggle" })
+
+-- Gitsigns keybindings
+map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview git hunk" })
+map("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle line blame" })
+map("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "Blame current line" })
+map("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" })
+map("n", "<leader>gn", ":Gitsigns next_hunk<CR>", { desc = "Next hunk" })
+map("n", "<leader>gN", ":Gitsigns prev_hunk<CR>", { desc = "Previous hunk" })
+map("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { desc = "Stage hunk" })
+
+-- Fugitive keybindings
+map("n", "<leader>gs", ":Git<CR>", { desc = "Git status" })
+map("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
+map("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "Git diff" })
+map("n", "<leader>gl", ":Git log<CR>", { desc = "Git log" })
+map("n", "<leader>gbb", ":Git blame<CR>", { desc = "Git blame" })
+map("n", "<leader>gP", ":Git push<CR>", { desc = "Git push" })
+map("n", "<leader>gA", ":Git add .<CR>", { desc = "Git add all files" })
